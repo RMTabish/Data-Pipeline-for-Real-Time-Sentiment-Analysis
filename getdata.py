c0f32c0f078a855baa260b8d025b48db1088bcc5
RMTabish <raimuhammadtabish@gmail.com>
2024-08-20 21:25:37 +0500

    cant connect to twitter

--- getdata.py
+++ getdata.py
@@ -1,17 +1,25 @@
-
 import tweepy
 import pandas as pd 
 from pymongo import MongoClient, ASCENDING, TEXT
 
-
+# Twitter API credentials
 access_token='1129351733715570688-CblRgAL7R5Pk0yFzS3QMaVccEgs8Vq'
 access_secret='Sy7yLyyQEVATDKja1eg0pJKZFiMevHq2j5G9LHq3rNKMA'
 
 consumer_key='2rFTPEzyUKyXdvgjsDj5zd5T6'
 consumer_secret='n0zVa1iDaWr3rDmxfHerqaN5I3Cdtq06L5OV4ef9NxXAIOycJc'
 
-#connecting with mongodb cluster
+# Accessing Twitter 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
 
-mongodb_conn=MongoClient('mongodb+srv://raimuhammadtabish:Z7tfY4E0kQQidP3B@twittersentiment.arfyts3.mongodb.net/?retryWrites=true&w=majority&appName=twitterSentiment')
-db=mongodb_conn.demo
-tweet_collection=db.tweet_collection
+# Increasing timeout limit and setting retries
+api = tweepy.API(auth, wait_on_rate_limit=True, timeout=300, retry_count=3, retry_delay=5, retry_errors=set([503]))
+
+try:
+    api.verify_credentials()
+    print("Authenticated")
+except tweepy.TweepyException as e:
+    print(f"Error during authentication: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
